398c420101612435c97750619c1aa827731394ca
Mads Ohmsen <Ohmsen93@hotmail.com>
2023-01-12 15:44:12 +0100

    Script Classes 1

--- Module_1_Js_Fundamentals/js/script.js
+++ Module_1_Js_Fundamentals/js/script.js
@@ -1,5 +1,56 @@
-let testobject = document.getElementById("testobject");
+class bank {
+    constructor(balance, loan, loanCount, loanBalance){
+        this.balance = balance;
+        this.loan = loan;
+        this.loanCount = loanCount;
+        this.loanBalance = loanBalance;
+    }
 
-testobject.addEventListener("click", function(){
-    testobject.style.backgroundColor = "yellow";
-});
+    getBalance(){
+        return this.balance;
+    }
+
+    updateBalance(ammount){
+        this.balance = this.balance + ammount;
+    }
+
+    getLoan(balance, loan){
+        
+        //if loanCount is 0, perform loan, else refuse loan.
+        if(this.loanCount === 0){
+            if(loan <= balance*2){
+                this.updateBalance(loan);
+                ++this.loanCount;
+            } else {
+                console.log("error, loan sum to large");
+            }
+
+        } else {
+            console.log("error, loan already taken");
+        }
+    }
+
+    getLoanBalance(){
+        return this.loanBalance;
+    }
+}
+
+class work {
+
+}
+
+class laptops{
+
+}
+
+
+// Here we write the function to populate our html objects with the values from our javascript
+function initializePageValues(){
+    
+    // we access the banking values through the id on the div
+    document.getElementById("balance").innerText.replace(bank.getBalance);
+}
+
+
+
+initializePageValues();
--- Module_1_Js_Fundamentals/public_html/index.php
+++ Module_1_Js_Fundamentals/public_html/index.php
@@ -3,28 +3,34 @@
 
 ?>
 
-
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Computer Store</title>
-    <script defer src="../js/script.js"></script>
-    <link rel="stylesheet" href="../css/stylesheet.css">
-</head>
-<body>
-    <div id="wrapper">
+
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+        <script defer src="../js/script.js"></script>
+    </head>
+    <body>
         <header>
-            <h1>Computer store</h1>
+        <!-- place navbar here -->
         </header>
+        <main>
+            <header>
+            <h1>Computer store</h1>
+            </header>
             <div id="content">
                 <div class="grid">
-                    <div class="gridTile" id="banker">
+                    <div class="gridTile" id="bank">
                         <h2>Joe Banker</h2>
-                        <p>Balance 200 Kr.</p>
+                        <p id="balance">Balance 200 Kr.</p>
                         <button>Get a loan</button>
                     </div>
                     <div class="gridTile" id="work">
@@ -43,28 +49,30 @@
                         <h4>Features:</h4>
                         <p>Description of the laptop</p>
                     </div>
-                    
                     <div class="gridTile" id="product">
                         <img id="productImg" src="https://dummyimage.com/100/00ff48/ff0000.png&text=Image+Found" />
-                            <div id="productText">
-                                <h3 id="productTitle" >Product Title</h3>
-                                <p id="productDesc">product Description, this is the description of the product. it should not take up overly much space and should be limited to the middle of the div right below the title.</p>
-                            </div>
-                            <div id="productPurchase">
-                                <h3 id="productPrice">1500 DKK</h3>
-                                <button id="productBtn">BUY NOW</button>
-                            </div>
-
-                           
-                        
-
-
-
-
-
+                        <div id="productText">
+                            <h3 id="productTitle" >Product Title</h3>
+                            <p id="productDesc">product Description, this is the description of the product. it should not take up overly much space and should be limited to the middle of the div right below the title.</p>
+                        </div>
+                        <div id="productPurchase">
+                            <h3 id="productPrice">1500 DKK</h3>
+                            <button id="productBtn">BUY NOW</button>
+                        </div>
                     </div>
                 </div>
-        </div>
-    </div>
-</body>
+            </div>
+        </main>
+        <footer>
+        <!-- place footer here -->
+        </footer>
+        <!-- Bootstrap JavaScript Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+        </script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+        </script>
+    </body>
 </html>
