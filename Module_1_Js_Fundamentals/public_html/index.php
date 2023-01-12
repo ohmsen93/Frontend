5da7bb8c8102c1fedad4851ad3bd915681504e56
Mads Ohmsen <Ohmsen93@hotmail.com>
2023-01-12 15:44:06 +0100

    Script Classes

--- Module_1_Js_Fundamentals/public_html/index.php
+++ Module_1_Js_Fundamentals/public_html/index.php
@@ -23,23 +23,43 @@
             <div id="content">
                 <div class="grid">
                     <div class="gridTile" id="banker">
-                        Banker
+                        <h2>Joe Banker</h2>
+                        <p>Balance 200 Kr.</p>
+                        <button>Get a loan</button>
                     </div>
                     <div class="gridTile" id="work">
-                        work
+                        <h2>Work</h2>
+                        <p>Pay 0 Kr.</p>
+                        <button>Bank</button>
+                        <button>Work</button>
                     </div>
                     <div class="gridTile" id="laptops">
-                        Laptop
+                        <h2>Laptops</h2>
+                        <Select>
+                            <Option>Laptop 1</Option>
+                            <Option>Laptop 2</Option>
+                            <Option>Laptop 3</Option>
+                        </Select>
+                        <h4>Features:</h4>
+                        <p>Description of the laptop</p>
                     </div>
                     
                     <div class="gridTile" id="product">
                         <img id="productImg" src="https://dummyimage.com/100/00ff48/ff0000.png&text=Image+Found" />
-                        <h3 id="productTitle" >Product Title</h3>
-                        <p id="productDesc">product Description, this is the description of the product. it should not take up overly much space and should be limited to the middle of the div right below the title.</p>
-                        <h5 id="productPrice">1500 DKK</h5>
-                        <button id="productBtn">BUY NOW</button>
-                        div#hello
+                            <div id="productText">
+                                <h3 id="productTitle" >Product Title</h3>
+                                <p id="productDesc">product Description, this is the description of the product. it should not take up overly much space and should be limited to the middle of the div right below the title.</p>
+                            </div>
+                            <div id="productPurchase">
+                                <h3 id="productPrice">1500 DKK</h3>
+                                <button id="productBtn">BUY NOW</button>
+                            </div>
+
+                           
                         
+
+
+
 
 
                     </div>
